78d3683505879a0b545416f97a591c07820cfd0f
Dylan Harville <dr.dyloh@gmail.com>
2021-09-12 01:14:04 -0600

    stack/isPalindrome is passing

--- src/stack/isPalindrome.js
+++ src/stack/isPalindrome.js
@@ -14,12 +14,33 @@
 const Stack = require("../linked-list/stack");
 
 function isPalindrome(text) {
-  const cleanText = text.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  let cleanText = text.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
 
   // TODO: Write an O(n) algorithm that uses a stack to determine whether 
   //the given input text is palindrome or not.
+  if (cleanText.length === 0) return false;
+  const newStack = new Stack();
+  let middle = Math.floor(cleanText.length / 2);
+  console.log(middle);
+  //check for odd number of characters and remove center char if exists
+  if (cleanText.length % 2) {
+    cleanText = cleanText.slice(0, middle) + cleanText.slice(middle + 1);
+    //middle += 1;
+  }
 
-  return false;
+  for (let i = 0; i < cleanText.length; i++) {
+    if (i < middle) {
+      newStack.push(cleanText[i]);
+    }
+    //while
+    if (i >= middle) {
+      if (newStack.pop() !== cleanText[i]){
+        return false;//true
+      }
+    }
+  }
+
+  return true;//false
 }
 
 module.exports = isPalindrome;
